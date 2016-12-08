988e74ee3cc20d2c8e0007a82275907287958b6c
syllogismos <anilkaraka@live.in>
2016-12-09 00:38:31 +0530

    shell script to generate month_train_files

---
+++ scripts/month_train_script.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+grep -e '^2015-01-28' train_ver2.csv > train_2015_01_28.csv
+grep -e '^2015-02-28' train_ver2.csv > train_2015_02_28.csv
+grep -e '^2015-03-28' train_ver2.csv > train_2015_03_28.csv
+grep -e '^2015-04-28' train_ver2.csv > train_2015_04_28.csv
+grep -e '^2015-05-28' train_ver2.csv > train_2015_05_28.csv
+grep -e '^2015-06-28' train_ver2.csv > train_2015_06_28.csv
+grep -e '^2015-07-28' train_ver2.csv > train_2015_07_28.csv
+grep -e '^2015-08-28' train_ver2.csv > train_2015_08_28.csv
+grep -e '^2015-09-28' train_ver2.csv > train_2015_09_28.csv
+grep -e '^2015-10-28' train_ver2.csv > train_2015_10_28.csv
+grep -e '^2015-11-28' train_ver2.csv > train_2015_11_28.csv
+grep -e '^2015-12-28' train_ver2.csv > train_2015_12_28.csv
+grep -e '^2016-01-28' train_ver2.csv > train_2016_01_28.csv
+grep -e '^2016-02-28' train_ver2.csv > train_2016_02_28.csv
+grep -e '^2016-03-28' train_ver2.csv > train_2016_03_28.csv
+grep -e '^2016-04-28' train_ver2.csv > train_2016_04_28.csv
+grep -e '^2016-05-28' train_ver2.csv > train_2016_05_28.csv
